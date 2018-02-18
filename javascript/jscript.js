40f0709d1255213bc37cc0a73fb37910bdb3c803
IlseSoto <ilsead_94@hotmail.com>
2018-02-17 22:42:08 -0600

    agregado js y playlist

---
+++ javascript/jscript.js
@@ -0,0 +1,15 @@
+function load(){
+	document.getElementById("div3_2").innerHTML='<h1>videos</h1><br><iframe width="560" height="315" src="https://www.youtube.com/embed/0rJ5debKSX0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
+function load0(){
+	//document.getElementByID("div3_2").innerHTML="";
+	document.getElementById("div3_2").innerHTML='<h1>videos</h1><br><iframe width="560" height="315" src="https://www.youtube.com/embed/2atx1Jh6YP0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
+
+function load1(){
+	document.getElementById("div3_2").innerHTML='<h1>videos</h1><br><iframe width="560" height="315" src="https://www.youtube.com/embed/LWGJA9i18Co" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
+
+function load2(){
+	document.getElementById("div3_2").innerHTML='<h1>videos</h1><br><iframe width="560" height="315" src="https://www.youtube.com/embed/SADub7W22Zg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
