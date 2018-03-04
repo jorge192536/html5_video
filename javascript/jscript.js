8bcba8771f8cf33d28f8ab13a8807c01c5bb416f
IlseSoto <ilsead_94@hotmail.com>
2018-03-04 16:53:33 +0000

    #1 agregado drag and drop de videos

--- javascript/jscript.js
+++ javascript/jscript.js
@@ -31,15 +31,15 @@ function login() {
     var account = localStorage.getItem("cuentas");
     document.getElementById("login").innerHTML = 'Bienvenid@ ' + account + '!';
 }
-
-function dragstart_handler(ev) {
+//AAAA
+/*function dragstart_handler(ev) {
     // Add the target element's id to the data transfer object
     ev.dataTransfer.setData("text/plain", ev.target.id);
     ev.dropEffect = "move";
 }
 
 function dragover_handler(ev) {
-    ev.preventDefault();
+    //ev.preventDefault();
     ev.dataTransfer.dropEffect = "move"; // move copy
 }
 
@@ -47,5 +47,39 @@ function drop_handler(ev) {
     ev.preventDefault();
     // Get the id of the target and add the moved element to the target's DOM
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    ev.target.appendChild(document.getElementById(data);
+}*/
+
+///D&D
+function start(){
+    showVid=document.getElementById("div3_2");
+    showVid.addEventListener('dragover', function(e){
+        e.preventDefault();
+    }, false);
+
+    showVid.addEventListener('dragover',function(e){
+        showVid.style.background = "#685bdd";
+        e.preventDefault();
+    },false);
+
+    showVid.addEventListener("drop", droppedElmt, false);
 }
+
+function droppedElmt(e){
+    e.preventDefault();
+    showVid.style.background = "#F85032";
+    var files=e.dataTransfer.files;
+    var file=files[0];
+    var reader= new FileReader();
+
+    reader.onload=(function(data){
+        return function (e2){
+            showVid.innerHTML=['<video width="720" height="404" controls autoplay type="video/x-matroska" src="', e2.target.result,
+                        '" title="', escape(data.name), '"/>'].join('');
+        };
+    }) (file);
+    reader.readAsDataURL(file);
+
+}
+
+window.addEventListener('load', start, false);
